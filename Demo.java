8e3b67a6766f195a5c3be31e60ff46cc179731ea
Venkey-project <vmamillapalli2@gmail.com>
2022-12-27 20:05:07 +0530

    updated Demo.java. one more line added

--- Demo.java
+++ Demo.java
@@ -4,5 +4,6 @@ class Demo
  {
    System.out.println("it is our first git repository");
    System.out.println("new line added to a demo");
+   System.out.println("doing changes on demo class");
   }
 }
