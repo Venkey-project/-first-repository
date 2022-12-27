cabab0936ea5b99cbc87e947827c714659f8c875
Venkey-project <121452404+Venkey-project@users.noreply.github.com>
2022-12-27 20:24:04 +0530

    Update Demo2.java

--- Demo2.java
+++ Demo2.java
@@ -3,6 +3,6 @@ class Demo2
  public Static void main(String[] args) 
  {
    System.out.println("it is our second-version");
-   
+   System.out.println("new branch is added in origin/master i.e, master-2");
   }
 }
